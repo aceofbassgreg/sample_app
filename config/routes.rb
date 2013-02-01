f2612963b95e4640d03579f1525469c4400cd56c
aceofbassgreg <gregory.kenenitz@gmail.com>
2013-02-01 14:46:25 -0500

    Add user microposts

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ SampleApp::Application.routes.draw do
   resources :users #This endows the application with all the actions needed (associated with
                 #the fundamental operations POST, GET, PUT, AND DELETE) for RESTful Users resource.
   resources :sessions, only: [:new, :create, :destroy]
+  resources :microposts, only: [:create, :destroy]
+  
   root to: 'static_pages#home'
   match '/signup', to: 'users#new'
   match '/signin', to: 'sessions#new'
